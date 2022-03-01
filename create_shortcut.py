2cf180da1e448506a21333eb0036398309ec7dcf
CaAnd <68998429+CaandRev@users.noreply.github.com>
2022-03-02 00:01:27 +0100

    Opprettelse av GUI

--- create_shortcut.py
+++ create_shortcut.py
@@ -1,54 +1,52 @@
 #  Skrevet av: CaAnd
 # Kan brukes fritt som det er uten noe ansvar
 
-import imp
 import os
-from pathlib import Path
+# from pathlib import Path
 from win32com.client import Dispatch
 import runtime
 
 # Variable
-
 info = 0
 
-#Input
-oppdragsnr = input('Inntast oppdragsnr: ')
-beskrivelse = input('Beskrivelse av oppdraget: ')
+while info == 0:
+    
+    oppdragsnr = input('Inntast oppdragsnr: ')
+    beskrivelse = input('Beskrivelse av oppdraget: ')
 
-start = runtime.starttime()
+    start = runtime.starttime()
 
+    path = 'X:\\nor\\oppdrag'
+    regioner = os.scandir(path)
+        
+    user = os.getlogin()
+    path ='C:\\Users\\' + user + '\\OneDrive - Norconsult Group\\Documents\\0_Oppdrag\\'+ oppdragsnr + ' - ' + beskrivelse +  '.lnk'
 
-path = 'X:\\nor\\oppdrag'
-regioner = os.scandir(path)
-     
-user = os.getlogin()
-path ='C:\\Users\\' + user + '\\OneDrive - Norconsult Group\\Documents\\0_Oppdrag\\'+ oppdragsnr + ' - ' + beskrivelse +  '.lnk'
+    print('Jobber med saken')
+    # Sjekker om destinasjons mappen eksistere
+    endposition = path.rfind('\\')
+    shortpath = path[:endposition+1]
 
-print('Jobber med saken')
-# Sjekker om destinasjons mappen eksistere
-endposition = path.rfind('\\')
-shortpath = path[:endposition+1]
+    if not os.path.exists(shortpath):
+        os.mkdir(shortpath)
+        finishpath = shortpath + 'Avsluttet oppdrag\\'
+        os.mkdir(finishpath)
 
-if not os.path.exists(shortpath):
-    os.mkdir(shortpath)
-    finishpath = shortpath + 'Avsluttet oppdrag\\'
-    os.mkdir(finishpath)
+    # Finn riktig region
+    for region in regioner:
+        target = 'X:\\nor\\oppdrag\\' + region.name + '\\'+ oppdragsnr[:3] + '\\' + oppdragsnr[3:5] + '\\' + oppdragsnr
+        # targetpath = Path(target)
 
-# Finn riktig region
-for region in regioner:
-    target = 'X:\\nor\\oppdrag\\' + region.name + '\\'+ oppdragsnr[:3] + '\\' + oppdragsnr[3:5] + '\\' + oppdragsnr
-    targetpath = Path(target)
+    #Lager snarvei    
+        if os.path.exists(target):
+            shell = Dispatch('WScript.Shell')
+            shortcut =  shell.CreateShortCut(path)
+            shortcut.Targetpath = target
+            shortcut.save()
+            info += 1
 
-#Lager snarvei    
-    if targetpath.exists():
-        shell = Dispatch('WScript.Shell')
-        shortcut =  shell.CreateShortCut(path)
-        shortcut.Targetpath = target
-        shortcut.save()
-        info += 1
-
-if info == 0:
-    print('Oppdragsmappen eksistere ikke')
+    if info == 0:
+        print('Oppdragsmappen eksistere ikke')
 
 runtime.stoptime(start)
-
+input('Push ENTER to finish')
